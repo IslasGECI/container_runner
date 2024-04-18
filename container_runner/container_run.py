5e8aea611773a5b29809b33bb801cf8f3a51d927
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-18 19:58:30 +0000

    ✅ Pass tests

--- container_runner/container_run.py
+++ container_runner/container_run.py
@@ -1,4 +1,4 @@
-def write_docker_command(password, container, target, path, image, username="analislas"):
+def write_docker_command(container, target, path, image, password, username="analislas"):
     return f'docker run \
     --env BITBUCKET_PASSWORD={password} \
     --env BITBUCKET_USERNAME={username} \
