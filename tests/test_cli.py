e0cd1763889d220f418573a2b54ce952f0febc4b
Mario Villasante <mario.villasante@islas.org.mx>
2024-04-18 15:05:52 -0700

    🎨 Pass format with black

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,12 +9,25 @@ def test_run_container():
     result = runner.invoke(cli, "--help")
     assert result.exit_code == 0
 
-
-    #obtained = write_docker_command( container, path, password)
+    # obtained = write_docker_command( container, path, password)
     image = "islasgeci/hola"
     container = "testmake_hola_develop"
     target = "mundo"
     path = "~/.testmake/hola"
-    result = runner.invoke(cli, [ "run-container", "--image", image, "--target", target, "--path", path, "--container", container, "--password", password])
+    result = runner.invoke(
+        cli,
+        [
+            "run-container",
+            "--image",
+            image,
+            "--target",
+            target,
+            "--path",
+            path,
+            "--container",
+            container,
+            "--password",
+            password,
+        ],
+    )
     assert result.exit_code == 0
-
