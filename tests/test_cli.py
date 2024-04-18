d04079bd3e13e39040679929fa028ed4f037ce7b
Mario Villasante <mario.villasante@islas.org.mx>
2024-04-18 15:01:13 -0700

    🚧💥 test run container cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,3 +8,13 @@ runner = CliRunner()
 def test_run_container():
     result = runner.invoke(cli, "--help")
     assert result.exit_code == 0
+
+
+    #obtained = write_docker_command( container, path, password)
+    image = "islasgeci/hola"
+    container = "testmake_hola_develop"
+    target = "mundo"
+    path = "~/.testmake/hola"
+    result = runner.invoke(cli, [ "run-container", "--image", image, "--target", target, "--path", path, "--container", container, "--password", password])
+    assert result.exit_code == 0
+
