d2566a37d32f87e591e048830651786529121590
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-04-18 19:43:08 +0000

    ♻️  Refactor

--- tests/test_container_run.py
+++ tests/test_container_run.py
@@ -5,24 +5,24 @@ def test_write_docker_command():
     password = "password"
     container = "new_container"
     target = "$OBJETIVO"
+    path = "new_path"
     expected = 'docker run \
     --env BITBUCKET_PASSWORD=password \
     --env BITBUCKET_USERNAME=analislas \
     --name new_container \
     --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
-    --volume path:/workdir \
+    --volume new_path:/workdir \
     image bash -c "\
       umask 000; \
       make $OBJETIVO \
         && echo $(date) > .make_succeeded \
         || rm --force .make_succeeded"'
-    obtained = write_docker_command(password, container, target)
+    obtained = write_docker_command(password, container, target, path)
     assert obtained == expected
 
     password = "new_password"
     username = "new_username"
-    path = "new_path"
     obtained = write_docker_command(password, container, target, path, username)
     assert username in obtained
     assert password in obtained
